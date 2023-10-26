89924cd0c18d011d0b812a7bdb1d153b107694af
GudvinPRO <138712942+GudvinPRO@users.noreply.github.com>
2023-10-26 22:38:36 +0300

    1

--- task-4.js
+++ task-4.js
@@ -1,10 +1,26 @@
-function calculateEvenTotal(number) {
-    let res = 0;
-    for (let i = 0; i <= number; i += 2) {   
-     res += i; } return res;
- }
- console.log(calculateEvenTotal(1));
- console.log(calculateEvenTotal(3));
- console.log(calculateEvenTotal(7));
- console.log(calculateEvenTotal(18));
- console.log(calculateEvenTotal(27));
+function getShippingCost(country) {
+    switch (country) {
+        case `China`:
+            return `Shipping to China will cost 100 credits`;
+        break;
+        case `Chile`: 
+            return `Shipping to Chile will cost 250 credits`;
+        break;
+        case `Australia`:
+            return `Shipping to Australia will cost 170 credits`;
+        break;
+        case `Jamaica`:
+            return `Shipping to Jamaica will cost 120 credits`;
+        break;
+        default: return `Sorry, there is no delivery to your country`;       
+    }
+}
+
+
+
+ console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
